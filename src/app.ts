45ec517d81a71ba49eca0e2c3f14077cf71e01af
tohedul-islam-nirzon <tohedul-islam-nirzon@users.noreply.github.com>
2026-05-23 12:33:37 +0600

    wire up routes in app

---
+++ src/app.ts
@@ -0,0 +1,35 @@
+import cors from "cors";
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+import config from "./config";
+import globalErrorHandler from "./middleware/globalErrorHandler";
+import { authRoute } from "./modules/auth/auth.route";
+import { issueRoute } from "./modules/issue/issue.route";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: config.cors_origin,
+  }),
+);
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "DevPulse API is running",
+  });
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/issues", issueRoute);
+
+app.use(globalErrorHandler);
+
+export default app;
