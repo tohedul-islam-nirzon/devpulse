4260caaa6271fb93f2d2f695b01bf21d145e8417
tohedul-islam-nirzon <tohedul-islam-nirzon@users.noreply.github.com>
2026-05-23 12:33:37 +0600

    add auth middleware to verify jwt token

---
+++ src/middleware/auth.ts
@@ -0,0 +1,66 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { pool } from "../db";
+import type { ROLES } from "../types";
+
+const auth = (...roles: ROLES[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // 1. Check if the token exists
+      // 2. Verify the token
+      // 3. Find the user in database
+      // 4. Check role permissions
+
+      const token = req.headers.authorization;
+
+      if (!token) {
+        return res.status(401).json({
+          success: false,
+          message: "You are not authorized!",
+        });
+      }
+
+      const decoded = jwt.verify(token, config.jwt_secret) as JwtPayload;
+
+      const userData = await pool.query(
+        `
+        SELECT * FROM users WHERE id=$1
+        `,
+        [decoded.id],
+      );
+
+      if (userData.rows.length === 0) {
+        return res.status(401).json({
+          success: false,
+          message: "User not found!",
+        });
+      }
+
+      const user = userData.rows[0];
+
+      if (roles.length && !roles.includes(user.role)) {
+        return res.status(403).json({
+          success: false,
+          message: "Forbidden! You don't have permission.",
+        });
+      }
+
+      req.user = {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      };
+
+      next();
+    } catch (error) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid or expired token!",
+      });
+    }
+  };
+};
+
+export default auth;
